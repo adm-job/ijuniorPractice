0d8816ece04502f5b937b62c8ccb831413e7e46e
Stanislav <adm.job@yandex.ru>
2025-04-24 00:40:11 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ijuniorPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2025;
+            float money  = 12.75f;
+            bool flag = true;
+            string nickname = "Destroyer";
+            char separator = ',';
+            string mail = "mail@mail.ru";
+            bool isEnd = false;
+            int randomDice = 6;
+            double playerSpeed = 55.5;
+            char empty = ' ';
+
+        }
+    }
+}
