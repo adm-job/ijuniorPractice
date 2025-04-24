41a462ee6ee28b3e8769f49628225b2eff8eb684
Stanislav <adm.job@yandex.ru>
2025-04-25 02:41:39 +0500

    modify code

--- Program.cs
+++ Program.cs
@@ -1,19 +1,35 @@
-﻿namespace ijuniorPractice
+﻿using System.Xml.Linq;
+
+namespace ijuniorPractice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int year = 2025;
-            float money  = 12.75f;
-            bool flag = true;
-            string nickname = "Destroyer";
-            char separator = ',';
-            string mail = "mail@mail.ru";
-            bool isEnd = false;
-            int randomDice = 6;
-            double playerSpeed = 55.5;
-            char empty = ' ';
+            int age;
+            string firstName;
+            string lastName;
+            string email;
+            float height;
+            string phoneNumber;
+            string workplace;
+
+            Console.Write("Введите ваше имя: ");
+            firstName = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            lastName = Console.ReadLine();
+            Console.Write("Сколько вам лет: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите ваш рост: ");
+            height = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Где вы работаете: ");
+            workplace = Console.ReadLine();
+            Console.Write("Укажите ваш номер телефона: ");
+            phoneNumber = Console.ReadLine();
+            Console.Write("Введите ваш email : ");
+            email = Console.ReadLine();
+
+            Console.WriteLine($"Вас зовут {firstName} {lastName}, вам {age}, ваш рост {height}, работаете на {workplace}, ваш номер телефона {phoneNumber}, email: {email}.");
 
         }
     }
