ca2323d6923ebec12f682e66773cfd4303f02d28
Stas <adm.job@yandex.ru>
2025-04-28 13:38:45 +0500

    update to menu

--- Program.cs
+++ Program.cs
@@ -10,22 +10,43 @@ namespace ijuniorPractice
         static void Main(string[] args)
         {
             Random random = new Random();
-            int number;
-            int sumNumbers = 0;
-            int divisorIs3 = 3;
-            int divisorIs5 = 5;
-            number = random.Next(1,101);
+            string numberInput = "";
+            int numberRandom = random.Next(0,1001);
 
-            for (int i = 1; i <= number; i++) 
+            const string CommandShowText = "1";
+            const string CommandRandomNumber = "2";
+            const string CommandClearConsole = "3";
+            const string CommandExit = "4";
+
+            while (numberInput != CommandExit)
             {
-                if ((i % divisorIs3 == 0) || (i % divisorIs5 == 0))
-                { 
-                    sumNumbers += i;
-                    Console.Write(i + " ");
+                Console.WriteLine("\nМеню");
+                Console.WriteLine($"{CommandShowText}. Вывод текста на консоль");
+                Console.WriteLine($"{CommandRandomNumber}. Вывод случайного числа от 0 до 1000");
+                Console.WriteLine($"{CommandClearConsole}. Очистка консоли");
+                Console.WriteLine($"{CommandExit}. Выход");
+                Console.Write("Введите пункт меню: ");
+                numberInput = Console.ReadLine();
+
+                switch (numberInput)
+                {
+                    case CommandShowText:
+                        Console.WriteLine("Просто бессмыссленный текст");
+                        Console.WriteLine("Дополнительный текст к первому сообщению");
+                        Console.WriteLine("=)");
+                        break;
+                    case CommandRandomNumber:
+                        Console.WriteLine("" + numberRandom);
+                        break;
+                    case CommandClearConsole:
+                        Console.Clear(); 
+                        break;
+                    case CommandExit:   
+                        break;
                 }
+
             }
-            Console.WriteLine("\nВыпавешее сисло " + number);
-            Console.WriteLine($"Сумма чисел кратные 3 или 5 равна {sumNumbers}");
+            Console.WriteLine("До свидания");
         }
     }
 }
