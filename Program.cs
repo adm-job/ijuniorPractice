c57d69735c6d7eccfaccdaadc94f1b4da5aa0b9c
Stanislav <adm.job@yandex.ru>
2025-04-29 01:43:38 +0500

    nag number

--- Program.cs
+++ Program.cs
@@ -9,20 +9,23 @@ namespace ijuniorPractice
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            string numberInput = "";
-            int numberRandom = random.Next(0,1001);
-
             const string CommandShowText = "1";
             const string CommandRandomNumber = "2";
             const string CommandClearConsole = "3";
             const string CommandExit = "4";
 
+            int startRandomNumber = 0;
+            int finisfRandomNumber = 1000;
+            int numberRandom;
+            string numberInput = "";
+
+            Random random = new Random();
+
             while (numberInput != CommandExit)
             {
                 Console.WriteLine("\nМеню");
                 Console.WriteLine($"{CommandShowText}. Вывод текста на консоль");
-                Console.WriteLine($"{CommandRandomNumber}. Вывод случайного числа от 0 до 1000");
+                Console.WriteLine($"{CommandRandomNumber}. Вывод случайного числа от {startRandomNumber} до {finisfRandomNumber}");
                 Console.WriteLine($"{CommandClearConsole}. Очистка консоли");
                 Console.WriteLine($"{CommandExit}. Выход");
                 Console.Write("Введите пункт меню: ");
@@ -36,6 +39,7 @@ namespace ijuniorPractice
                         Console.WriteLine("=)");
                         break;
                     case CommandRandomNumber:
+                        numberRandom = random.Next(startRandomNumber, finisfRandomNumber);
                         Console.WriteLine("" + numberRandom);
                         break;
                     case CommandClearConsole:
