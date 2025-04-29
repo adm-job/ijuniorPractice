78c0784cae5593482f9b03d7092eaf60ba351b52
Stas <adm.job@yandex.ru>
2025-04-29 16:33:05 +0500

    exchange office

--- Program.cs
+++ Program.cs
@@ -9,48 +9,123 @@ namespace ijuniorPractice
     {
         static void Main(string[] args)
         {
-            const string CommandShowText = "1";
-            const string CommandRandomNumber = "2";
-            const string CommandClearConsole = "3";
-            const string CommandExit = "4";
+            const string CommandRubToUsd = "1";
+            const string CommandRubToEur = "2";
+            const string CommandUsdToEur = "3";
+            const string CommandEurToUsd = "4";
+            const string CommandUsdToRub = "5";
+            const string CommandEurToRub = "6";
+            const string CommandExit = "7";
 
-            int startRandomNumber = 0;
-            int finisfRandomNumber = 1000;
-            int numberRandom;
-            string numberInput = "";
+            float amountRub = 1000;
+            float amountUsd = 100;
+            float amountEur = 50;
+            float rateRubToUsd = 90;
+            float rateRubToEur = 100;
+            float rateUsdToEur = 1.11f;
+            float amountExchange;
+            string inputComand;
+            string errorMassage = "На счету нет такой суммы";
 
-            Random random = new Random();
+            bool isOpen = true;
 
-            while (numberInput != CommandExit)
+            while (isOpen)
             {
-                Console.WriteLine("\nМеню");
-                Console.WriteLine($"{CommandShowText}. Вывод текста на консоль");
-                Console.WriteLine($"{CommandRandomNumber}. Вывод случайного числа от {startRandomNumber} до {finisfRandomNumber}");
-                Console.WriteLine($"{CommandClearConsole}. Очистка консоли");
-                Console.WriteLine($"{CommandExit}. Выход");
-                Console.Write("Введите пункт меню: ");
-                numberInput = Console.ReadLine();
+                Console.WriteLine("Пункт обмена открыт!");
+                Console.WriteLine($"{CommandRubToUsd} Обмен RUB на USD");
+                Console.WriteLine($"{CommandRubToEur} Обмен RUB на EUR");
+                Console.WriteLine($"{CommandUsdToEur} Обмен USD на EUR");
+                Console.WriteLine($"{CommandEurToUsd} Обмен EUR на USD");
+                Console.WriteLine($"{CommandUsdToRub} Обмен USD на RUB");
+                Console.WriteLine($"{CommandEurToRub} Обмен EUR на RUB");
+                Console.WriteLine($"{CommandExit} Выход");
+                Console.WriteLine($"\n RUB {amountRub} \t USD {amountUsd} \t EUR {amountEur}\n");
 
-                switch (numberInput)
+                Console.WriteLine("Введите операцию обмена");
+                inputComand = Console.ReadLine();
+
+                if ( inputComand == CommandExit )
                 {
-                    case CommandShowText:
-                        Console.WriteLine("Просто бессмыссленный текст");
-                        Console.WriteLine("Дополнительный текст к первому сообщению");
-                        Console.WriteLine("=)");
+                    isOpen = false;
+                    continue;
+                }
+                
+                Console.WriteLine("Введите сумму для обмена");
+                amountExchange = Convert.ToSingle(Console.ReadLine());
+
+                switch (inputComand)
+                {
+                    case CommandRubToUsd:
+
+                        if (amountExchange > amountRub)
+                        {
+                            Console.WriteLine(errorMassage);
+                            break;
+                        }
+
+                        amountRub -= amountExchange;
+                        amountUsd += amountExchange / rateRubToUsd;
                         break;
-                    case CommandRandomNumber:
-                        numberRandom = random.Next(startRandomNumber, finisfRandomNumber);
-                        Console.WriteLine("" + numberRandom);
+                    case CommandRubToEur:
+
+                        if (amountExchange > amountRub)
+                        {
+                            Console.WriteLine(errorMassage);
+                            break;
+                        }
+
+                        amountRub -= amountExchange;
+                        amountEur += amountExchange / rateRubToEur;
                         break;
-                    case CommandClearConsole:
-                        Console.Clear(); 
+                    case CommandUsdToEur:
+
+                        if (amountExchange > amountUsd)
+                        {
+                            Console.WriteLine(errorMassage);
+                            break;
+                        }
+
+                        amountUsd -= amountExchange;
+                        amountEur += amountExchange / rateUsdToEur;
                         break;
-                    case CommandExit:   
+                    case CommandEurToUsd:
+
+                        if (amountExchange > amountEur)
+                        {
+                            Console.WriteLine(errorMassage);
+                            break;
+                        }
+
+                        amountEur -= amountExchange;
+                        amountUsd += amountExchange * rateUsdToEur;
                         break;
+                    case CommandUsdToRub:
+
+                        if (amountExchange > amountUsd)
+                        {
+                            Console.WriteLine(errorMassage);
+                            break;
+                        }
+
+                        amountUsd -= amountExchange;
+                        amountRub += amountExchange * rateRubToUsd;
+                        break;
+                    case CommandEurToRub:
+
+                        if (amountExchange > amountEur)
+                        {
+                            Console.WriteLine(errorMassage);
+                            break;
+                        }
+
+                        amountEur -= amountExchange;
+                        amountRub += amountExchange * rateRubToEur;
+                        break;
+
                 }
 
             }
-            Console.WriteLine("До свидания");
+
         }
     }
 }
