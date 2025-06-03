876ac05cbdbe6fd12c4efe8c81b58c9d386f975d
Stanislav <adm.job@yandex.ru>
2025-06-04 03:24:43 +0500

    add SearchDictionary

--- Program.cs
+++ Program.cs
@@ -4,23 +4,36 @@
     {
         static void Main(string[] args)
         {
-            // MainMenu();
-            static void Main(string[] args)
+            Dictionary<string, string> dictionaryMeanings = new Dictionary<string, string>();
+            string inputSearchString;
+
+            FillDictionary(ref dictionaryMeanings);
+
+            Console.Write("Введите слово для поиска: ");
+            inputSearchString = Console.ReadLine().ToLower();
+
+            SearchDictionary(dictionaryMeanings, inputSearchString);
+
+        }
+
+        static void FillDictionary(ref Dictionary<string, string> dictionary)
+        {
+            dictionary.Add("Синоним", "Слова одной части речи или словосочетания с полным или частичным совпадением значения");
+            dictionary.Add("Антоним", "Слова одной части речи, различные по звучанию и написанию, имеющие прямо противоположные лексические значения, например: «правда» — «ложь»");
+            dictionary.Add("Омоним", "Одинаковые по написанию и звучанию, но разные по значению слова и другие единицы языка");
+            dictionary.Add("Пароним", "Слова, сходные по звучанию и морфемному составу, но различающиеся лексическим значением");
+            dictionary.Add("Омофон", "Одинаково произносящиеся, но по-разному пишущиеся слова: плот и плод");
+        }
+
+        static void SearchDictionary(Dictionary<string, string> dictionary, string searchString)
+        {
+            if (dictionary.ContainsKey(searchString))
             {
-
-                Dictionary<string, string> dictionaryMeanings = new Dictionary<string, string>();
-
-
+                Console.WriteLine(dictionary[searchString]);
             }
-
-            static void FillDictionary(ref Dictionary<string, string> dictionary)
+            else
             {
-                dictionary.Add("Синоним", "Слова одной части речи или словосочетания с полным или частичным совпадением значения");
-                dictionary.Add("Антоним", "Слова одной части речи, различные по звучанию и написанию, имеющие прямо противоположные лексические значения, например: «правда» — «ложь»");
-                dictionary.Add("Омоним", "Одинаковые по написанию и звучанию, но разные по значению слова и другие единицы языка");
-                dictionary.Add("Пароним", "Слова, сходные по звучанию и морфемному составу, но различающиеся лексическим значением");
-                dictionary.Add("Омофон", "Одинаково произносящиеся, но по-разному пишущиеся слова: плот и плод");
-                dictionary.Add("Омоним", "Одинаково произносящиеся и пишущиеся, но не связанные по значению слова: коса (вид причёски), коса (сельскохозяйственный инструмент), коса (узкая полоска земли, окружённая водой)");
+                Console.WriteLine("Данного слова нет в словаре");
             }
         }
     }
