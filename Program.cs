db53fbe5c3a1a9491c0c9d962218a0a165f075cb
Stanislav <adm.job@yandex.ru>
2025-06-08 09:38:46 +0500

    Queue

--- Program.cs
+++ Program.cs
@@ -1,40 +1,40 @@
-﻿namespace ijuniorPractice
+﻿using System;
+
+namespace ijuniorPractice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> dictionaryMeanings = new Dictionary<string, string>();
-            string inputSearchString;
+            Queue<int> amountBuyersReceipt = new Queue<int>();
+            int sumAllBuyers = 0;
 
-            FillDictionary(ref dictionaryMeanings);
+            amountBuyersReceipt = RandomAmountChecks();
 
-            Console.Write("Введите слово для поиска: ");
-            inputSearchString = Console.ReadLine().ToLower();
-
-            SearchDictionary(dictionaryMeanings, inputSearchString);
-
-        }
-
-        static void FillDictionary(ref Dictionary<string, string> dictionary)
-        {
-            dictionary.Add("Синоним", "Слова одной части речи или словосочетания с полным или частичным совпадением значения");
-            dictionary.Add("Антоним", "Слова одной части речи, различные по звучанию и написанию, имеющие прямо противоположные лексические значения, например: «правда» — «ложь»");
-            dictionary.Add("Омоним", "Одинаковые по написанию и звучанию, но разные по значению слова и другие единицы языка");
-            dictionary.Add("Пароним", "Слова, сходные по звучанию и морфемному составу, но различающиеся лексическим значением");
-            dictionary.Add("Омофон", "Одинаково произносящиеся, но по-разному пишущиеся слова: плот и плод");
-        }
-
-        static void SearchDictionary(Dictionary<string, string> dictionary, string searchString)
-        {
-            if (dictionary.ContainsKey(searchString))
+            foreach (int purchaseAmount in amountBuyersReceipt)
             {
-                Console.WriteLine(dictionary[searchString]);
+                Console.Clear();
+                Console.WriteLine($"Сумма покупки: {purchaseAmount}");
+                sumAllBuyers += purchaseAmount;
+                Console.WriteLine($"Всего на счету: {sumAllBuyers}\n");
+                Console.WriteLine("нажмите любую клавишу");
+                Console.ReadKey(); 
             }
-            else
+        }
+        static Queue<int> RandomAmountChecks()
+        {
+            Random randomInteger = new Random();
+            Queue<int> amountBuyersReceipt = new Queue<int>();
+
+            int queueSize = randomInteger.Next(15, 25);
+
+
+            for (int i = 0; i < queueSize; i++)
             {
-                Console.WriteLine("Данного слова нет в словаре");
+                amountBuyersReceipt.Enqueue(randomInteger.Next(100, 1000));
             }
+
+            return amountBuyersReceipt;
         }
     }
 }
