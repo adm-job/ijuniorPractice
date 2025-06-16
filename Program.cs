7b3b1cec4259242247bd5bb0abc7f0df584e910b
Stas <adm.job@yandex.ru>
2025-06-16 13:55:24 +0500

    update -  add List

--- Program.cs
+++ Program.cs
@@ -9,50 +9,59 @@ namespace ijuniorPractice
             const string CommandSum = "sum";
             const string CommandExit = "exit";
 
-            int[] numberInputAll = new int[0];
-            int[] numberTemp;
-            int correction = 1;
+            List<int> numberList = new List<int>();
             int sumNumber = 0;
-            string numberInput;
+            string comandInput;
             bool isExit = false;
 
             while (isExit == false)
             {
-                numberTemp = new int[numberInputAll.Length + correction];
-
                 Console.WriteLine($"Введите число \nСумма чисел - {CommandSum}\nВыход - {CommandExit}");
-                numberInput = Console.ReadLine().ToLower();
+                comandInput = ReadInput();
                 Console.Clear();
 
-                switch (numberInput)
+                switch (comandInput)
                 {
                     case CommandExit:
                         isExit = true;
                         break;
-
                     case CommandSum:
                         Console.WriteLine($"Сумма введенных чисел: {sumNumber}\n");
                         break;
 
                     default:
-                        numberInputAll[numberInputAll.Length - correction] = Convert.ToInt32(numberInput);
+                        numberList.Add(Convert.ToInt32(comandInput));
+                        sumNumber += Convert.ToInt32(comandInput);
 
-                        foreach (int number in numberInputAll)
+                        foreach (int number in numberList)
                         {
                             Console.Write(number + " ");
                         }
 
                         Console.WriteLine("\n");
-                        sumNumber += numberInputAll[numberInputAll.Length - correction];
-
-                        for (int i = 0; i < numberInputAll.Length; i++)
-                        {
-                            numberTemp[i] = numberInputAll[i];
-                        }
-
-                        numberInputAll = numberTemp;
                         break;
                 }
+            }
+
+            static string ReadInput()
+            {
+                string input = "";
+                bool isCheck = true;
+
+                while (isCheck)
+                {
+                    input = Console.ReadLine();
+
+                    if (int.TryParse(input, out _) || input == "sum" || input == "exit")
+                    {
+                        isCheck = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не верный ввод");
+                    }
+                }
+                return input;
             }
         }
     }
