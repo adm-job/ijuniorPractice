6d22d943a90a1e8bd77b1dd8c4ce372399506a23
Stas <adm.job@yandex.ru>
2025-06-16 17:27:41 +0500

    start

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace ijuniorPractice
 {
@@ -6,6 +7,8 @@ namespace ijuniorPractice
     {
         static void Main(string[] args)
         {
+            //File.ReadAllLines(map.txt);
+
 
         }
     }
