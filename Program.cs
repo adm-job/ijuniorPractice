5f9ddb36c7e240b1552a4db80ee13d2f4910a5b3
Stas <adm.job@yandex.ru>
2025-06-16 09:34:52 +0500

    update dynamic input

--- Program.cs
+++ Program.cs
@@ -6,40 +6,54 @@ namespace ijuniorPractice
     {
         static void Main(string[] args)
         {
-            Queue<int> amountBuyersReceipt = new Queue<int>();
-            int sumAllBuyers = 0;
+            const string CommandSum = "sum";
+            const string CommandExit = "exit";
 
-            amountBuyersReceipt = FillingQueue();
+            int[] numberInputAll = new int[0];
+            int[] numberTemp;
+            int correction = 1;
+            int sumNumber = 0;
+            string numberInput;
+            bool isExit = false;
 
-            while(amountBuyersReceipt.Count > 0)
+            while (isExit == false)
             {
+                numberTemp = new int[numberInputAll.Length + correction];
+
+                Console.WriteLine($"Введите число \nСумма чисел - {CommandSum}\nВыход - {CommandExit}");
+                numberInput = Console.ReadLine().ToLower();
                 Console.Clear();
-                sumAllBuyers += amountBuyersReceipt.Peek();
-                Console.WriteLine($"Сумма покупки: {amountBuyersReceipt.Dequeue()}");
-                Console.WriteLine($"Всего на счету: {sumAllBuyers}\n");
-                Console.WriteLine("нажмите любую клавишу");
-                Console.ReadKey();
+
+                switch (numberInput)
+                {
+                    case CommandExit:
+                        isExit = true;
+                        break;
+
+                    case CommandSum:
+                        Console.WriteLine($"Сумма введенных чисел: {sumNumber}\n");
+                        break;
+
+                    default:
+                        numberInputAll[numberInputAll.Length - correction] = Convert.ToInt32(numberInput);
+
+                        foreach (int number in numberInputAll)
+                        {
+                            Console.Write(number + " ");
+                        }
+
+                        Console.WriteLine("\n");
+                        sumNumber += numberInputAll[numberInputAll.Length - correction];
+
+                        for (int i = 0; i < numberInputAll.Length; i++)
+                        {
+                            numberTemp[i] = numberInputAll[i];
+                        }
+
+                        numberInputAll = numberTemp;
+                        break;
+                }
             }
-        }
-
-        static Queue<int> FillingQueue()
-        {
-            Random random = new Random();
-            Queue<int> amountBuyersReceipt = new Queue<int>();
-
-            int minQueueSize = 15;
-            int maxQueueSize = 25;
-            int minAmount = 100;
-            int maxAmount = 1000;
-
-            int queueSize = random.Next(minQueueSize, maxQueueSize + 1);
-
-            for (int i = 0; i < queueSize; i++)
-            {
-                amountBuyersReceipt.Enqueue(random.Next(minAmount, maxAmount));
-            }
-
-            return amountBuyersReceipt;
         }
     }
 }
