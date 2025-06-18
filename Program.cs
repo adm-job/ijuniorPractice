97eef5820deb1db7d1f494d03b014441a0648c9b
Stanislav <adm.job@yandex.ru>
2025-06-19 00:13:03 +0500

    read map complete

--- Program.cs
+++ Program.cs
@@ -7,21 +7,46 @@ namespace ijuniorPractice
     {
         static void Main(string[] args)
         {
-            ReadMap("map.txt");
+            char[,] map = ReadMap("map.txt");
+            DrowMap(map);
         }
 
         private static char[,] ReadMap(string patch)
         {
             string[] file = File.ReadAllLines("map.txt");
-            char[,] map = new char[file[0].Length,file.Length] ;
+            char[,] map = new char[GetMaxLenghtOfLine(file), file.Length];
 
-            for (int x = 0; x < file.Length; x++)
+            for (int x = 0; x < map.GetLength(0); x++)
             {
-                for (int y = 0; y < file[x].Length; y++)
+                for (int y = 0; y < map.GetLength(1); y++)
                     map[x, y] = file[y][x];
             }
 
             return map;
+        }
+
+        private static void DrowMap(char[,] map)
+        {
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                for (int x = 0; x < map.GetLength(0); x++)
+                    Console.Write(map[x, y]);
+
+                Console.Write("\n");
+            }
+        }
+
+        private static int GetMaxLenghtOfLine(string[] lines)
+        {
+            int maxLenght = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Length > maxLenght)
+                    maxLenght = lines[i].Length;
+            }
+
+            return maxLenght;
         }
     }
 }
