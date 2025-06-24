43157308592ef1614aec946a54f72333587e697c
Stas <adm.job@yandex.ru>
2025-06-24 18:39:05 +0500

    first class Player

--- Program.cs
+++ Program.cs
@@ -4,42 +4,31 @@
     {
         static void Main(string[] args)
         {
-            List<int> generalList = new List<int>();
+            Player player1 = new Player();
+            Player player2 = new Player();
 
-            int[] differentNumbers1 = { 3, 9, 15, 6, 2, 9, 11, 1, 7, 2, 19, 4 };
-            int[] differentNumbers2 = { 1, 2, 6, 4, 18, 19, 5, 7, 8, 10, 6, 15, 1 };
+            player1.Name = "Иван";
+            player1.Description = "Заядлый игрок";
 
-            AddUniqueNumbers(generalList, differentNumbers1);
-            AddUniqueNumbers(generalList, differentNumbers2);
+            player2.Name = "Сергей";
+            player2.Description = "Начинающий";
 
-            Console.WriteLine("Обший не сортированный список");
-            OutputList(generalList);
+            player1.ShowInfo();
+            player2.ShowInfo();
 
-            generalList.Sort();
-
-            Console.WriteLine("Обший отсортированный список");
-            OutputList(generalList);
         }
 
-        static void AddUniqueNumbers(List<int> generalList, int[] list)
-        {
-            for (int i = 0; i < list.Length; i++)
-            {
-                if (generalList.Contains(list[i]) == false)
-                {
-                    generalList.Add(list[i]);
-                }
-            }
-        }
 
-        static void OutputList(List<int> generalList)
-        {
-            foreach (var number in generalList)
-            {
-                Console.Write(number + " ");
-            }
+    }
+    class Player
+    {
+        public string Name;
+        public string Description;
 
-            Console.WriteLine();
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя - {Name}");
+            Console.WriteLine($"Описание игрока - {Description}");
         }
     }
 }
