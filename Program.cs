8d1478126de566d70d035a801d5d53de368a574e
Stanislav <adm.job@yandex.ru>
2025-06-26 01:05:44 +0500

    working with properties

--- Program.cs
+++ Program.cs
@@ -4,50 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player("Иван", "Заядлый игрок", 507);
-            Player player2 = new Player("Сергей", "Начинающий", 5);
-            Player player3 = new Player();
+            Renderer renderer = new Renderer();
+            Player player = new Player(30,20 ,'$');
 
-            player1.ShowInfo();
-            player2.ShowInfo();
-            player3.ShowInfo();
+            renderer.Draw(player.X, player.Y, player.Chacter);
         }
     }
 
     class Player
     {
-        private string _name;
-        private string _description;
-        private int _gamesPlayed;
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public char Chacter {  get; private set; }
 
-        public Player()
+
+        public Player(int x, int y, char chacter)
         {
-            _name = "Пользователь";
-            _description = "Безымянный пользователь";
-            _gamesPlayed = 0;
+            X = x;
+            Y = y;
+            Chacter = chacter;
         }
+    }
 
-        public Player(string name, string description, int gamesPlayed)
+    public class Renderer
+    {
+        public void Draw( int x, int y , char chacter)
         {
-            _name = name;
-            _description = description;
-
-            if (gamesPlayed > 0)
-            {
-                _gamesPlayed = gamesPlayed;
-            }
-            else
-            {
-                gamesPlayed = 0;
-                _gamesPlayed = gamesPlayed;
-            }
-        }
-
-        public void ShowInfo()
-        {
-            Console.WriteLine($"Имя - {_name}");
-            Console.WriteLine($"Описание игрока - {_description}");
-            Console.WriteLine($"Игр сыграно - {_gamesPlayed}\n");
+            Console.SetCursorPosition(x, y);
+            Console.Write(chacter);
+            Console.ReadKey();
         }
     }
 }
