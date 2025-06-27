b346a315c8c829a2e04609713e03e16a744ba404
Stas <adm.job@yandex.ru>
2025-06-27 18:01:00 +0500

    Add user menu

--- Program.cs
+++ Program.cs
@@ -4,11 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Player player  =  new Player(1,"Benn", 55 , false);
+            Player player = new Player(1, "Benn", 55, false);
             Console.WriteLine(player.PlayerID);
             Console.WriteLine(player.Name);
             Console.WriteLine(player.PlayerLvl);
             Console.WriteLine(player.IsBanned);
+
+            const string comandAddPlayer = "Добавить нового игрока в базу";
+            const string comandShowAllPlayers = "Показать всех игроков в базе";
+            const string comandRemovePlayer = "Удаление игрока из базы";
+            const string comandbannedPlayer = "Забанить игрока";
+            const string comandExit = "Выход";
+
+            string inputUser;
+
+            do
+            {
+                inputUser = Console.ReadLine();
+
+                switch (player)
+                {
+
+                }
+
+            }
+            while (inputUser != comandExit);
+
+
         }
     }
 
